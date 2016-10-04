5676ba7af8b63aaf31433830c550936a0f6bf771
Maciej Sikora <msikora@dataart.com>
2016-10-04 16:11:19 +0200

    compiled script for presentation

---
+++ bin/1.app.bundle.js
@@ -0,0 +1,32 @@
+webpackJsonp([1],{
+
+/***/ 173:
+/***/ function(module, exports) {
+
+	"use strict";
+
+/***/ },
+
+/***/ 174:
+/***/ function(module, exports) {
+
+	"use strict";
+
+	var TaskList = React.createClass({
+	  displayName: "TaskList",
+
+
+	  render: function render() {
+
+	    return React.createElement(
+	      "h1",
+	      null,
+	      "Here tasks"
+	    );
+	  }
+
+	});
+
+/***/ }
+
+});
